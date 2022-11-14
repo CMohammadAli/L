c9dcd9b09f92f3567dcd08f41310aa10e0dc726a
Mohammad <shaikali2684@gmail.com>
2022-11-14 18:19:54 +0530

    First

---
+++ DatabaseProjectNEWDB/DatabaseProjectNEWDB.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[STUDENTS] (
+    [ID]      INT          NOT NULL,
+    [NAME]    VARCHAR (20) NOT NULL,
+    [AGE]     INT          NOT NULL,
+    [ADDRESS] CHAR (25)    NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+
